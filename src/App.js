0108a092a73d3bb938a0200fb72f075b1a25be93
Raghuveer Sharma Saripalli <rvs1604@gmail.com>
2022-02-07 19:19:50 -0500

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Nav Bar 
+    <AppBar position="static" color="primary">
+      <Grid container columns={16}>
+        <Grid item xs={3}>
+          <Toolbar>
+            <Typography variant="h5" component="div">
+              <Link href="" color="inherit" underline='none'>
+                {"(Raghu, Saripalli)"}
+              </Link>
+            </Typography>
+          </Toolbar>
+        </Grid>
+        <Grid item xs={1}>
+          <Toolbar>
+            <Typography variant="h5" component="div">
+              <div>{"=>"}</div>
+            </Typography>
+          </Toolbar>
+        </Grid>
+        <Grid item xs={1}>
+          <Toolbar>
+            <Typography variant="h5" component="div">
+              {"{"}
+            </Typography>
+          </Toolbar>
+        </Grid>
+        <Grid item xs={2}>
+          <Toolbar>
+            <Typography variant="h5" component="div">
+              <Link href="/skills" color="inherit" underline='none'>
+                {"Skills"}
+              </Link>
+            </Typography>
+          </Toolbar>
+        </Grid>
+        <Grid item xs={2}>
+          <Toolbar>
+            <Typography variant="h5" component="div">
+              <Link href="/edu" color="inherit" underline='none'>
+                {"Education"}
+              </Link>
+            </Typography>
+          </Toolbar>
+        </Grid>
+        <Grid item xs={2}>
+          <Toolbar>
+            <Typography variant="h5" component="div">
+              <Link href="/exp" color="inherit" underline='none'>
+                {"Experience"}
+              </Link>
+            </Typography>
+          </Toolbar>
+        </Grid>
+        <Grid item xs={2}>
+          <Toolbar>
+            <Typography variant="h5" component="div">
+              <Link href="/proj" color="inherit" underline='none'>
+                {"Projects"}
+              </Link>
+            </Typography>
+          </Toolbar>
+        </Grid>
+        <Grid item xs={2}>
+          <Toolbar>
+            <Typography variant="h5" component="div">
+              <Link href="/ach" color="inherit" underline='none'>
+                {"Achievements"}
+              </Link>
+            </Typography>
+          </Toolbar>
+        </Grid>
+        <Grid item xs={1}>
+          <Toolbar>
+            <Typography variant="h5" component="div">
+              {"}"}
+            </Typography>
+          </Toolbar>
+        </Grid>
+      </Grid>
+    </AppBar>
   );
 }
 
